8ff297a8dfc9da974acdaa8f2b22ba6173a870bb
CHROSW <oschristophe@gmail.com>
2023-09-25 17:00:15 +0200

    Ajout de la modale de contact

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 function enqueue_script_my_scripts(){
-    wp_enqueue_style('mycss', get_template_directory_uri() . 'style.css');
-    wp_enqueue_script('myscript', get_template_directory_uri() . '/js/scripts.js', array(), '1.0', true);
+    wp_enqueue_style('mycss', get_template_directory_uri() . '/style.css');
+    wp_register_script('myscript', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0', false);
+    wp_enqueue_script('myscript');
 }
 
 add_action('wp_enqueue_scripts', 'enqueue_script_my_scripts');
---
+++ templates_part/contact.php
@@ -0,0 +1,19 @@
+<!-- Trigger/Open The Modal -->
+<button id="myBtn">Contact</button>
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+<!-- Modal content -->
+    <div class="modal-content">
+        <div class="modal-header">
+            <span class="close">×</span>
+            <h2>CONTACTCONTACTCONTACTCONTACTCONTACTCONTACT</h2>
+        </div>
+        <div class="modal-body">
+            <?php echo do_shortcode('[contact-form-7 id="d0df9bd" title="Contact"]'); ?>
+        </div>
+        <div class="modal-footer">
+        </div>
+    </div>
+</div>

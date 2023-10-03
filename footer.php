ac7d6fd3d0d90731a20a6907cc108d2baff0a9ed
CHROSW <oschristophe@gmail.com>
2023-10-03 16:27:40 +0200

    ajout des modifications du fichier footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,10 @@
         <?php
         get_template_part('/templates_part/contact'); 
         ?>
+        <footer class="footer">
+            <nav role="navigation" aria-label="<?php _e('Footer Menu', 'text-domain'); ?>">
+                <?php wp_nav_menu(['theme_location' => 'footer-menu',]); ?>
+            </nav>
+        </footer>
     </body>
 </html>

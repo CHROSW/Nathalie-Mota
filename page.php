d59952c7dbb1208129776c6fdba16cba17c83026
CHROSW <oschristophe@gmail.com>
2023-10-20 18:01:10 +0200

    Ajout des fichiers pour le blog

--- page.php
+++ page.php
@@ -11,15 +11,15 @@
 
 get_header();
 
-/* Start the Loop */
+
 while ( have_posts() ) :
 	the_post();
-	get_template_part( 'template-parts/content/content-page' );
+	get_template_part( 'templates-part/content-page' );
 
 	// If comments are open or there is at least one comment, load up the comment template.
 	if ( comments_open() || get_comments_number() ) {
 		comments_template();
 	}
-endwhile; // End of the loop.
+endwhile;
 
 get_footer();
